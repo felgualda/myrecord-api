d7c65dfa08186fb7d6a02557f468032fecef9636
Felipe  Vieira Gualda Pereira <felipepereira_estag@intranet.leste>
2026-05-07 20:16:06 -0300

    Testes com o método POST

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,21 @@ app.get('/api/status', (req: Request, res: Response) => {
     });
 });
 
+app.post('/api/songs', (req: Request, res: Response) => {
+    const {title, artist} = req.body;
+
+    if (!title || !artist) {
+        return res.status(400).json({error: 'Titulo e artista obrigatórios'});
+    }
+
+    console.log(`Musica recebida: ${title} de ${artist}`);
+
+    res.status(201).json({
+        message: 'Música recebida com sucesso',
+        song: {title, artist, registeredAt: new Date() }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
