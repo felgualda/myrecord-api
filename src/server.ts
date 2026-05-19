f2a5f344435a4173e035943483d99b85b0715285
Felipe  Vieira Gualda Pereira <felipegualda@id.uff.br>
2026-05-19 16:00:54 -0300

    JWT Token, senha armazenada em hash e nome único para usuário

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express, { type Request, type Response } from 'express';
 import { prisma } from './lib/prisma.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { z } from 'zod';
+
 
 const app = express();
 const PORT = 3333;
@@ -68,19 +72,75 @@ app.get('/api/spotify/search', async (req: Request, res: Response) => {
     }
 });
 
-app.post('/api/users', async (req: Request, res: Response) => {
-    const {name, email, password } = req.body
-
-    if (!name || !email || !password) {
-        return res.status(400).json({error: 'Nome, email e senha obrigatórios'});
-    }
+app.get('/api/users/:username/records', async (req: Request, res: Response) => {
+    const { username } = req.params;
 
     try {
+        const userProfile = await prisma.user.findUnique({
+            where: { username },
+            select: {
+                id: true,
+                nickname: true,
+
+                records: {
+                    include: {
+                        song: true
+                    },
+                    orderBy: {
+                        registeredAt: 'desc'
+                    }
+                }
+            }
+        });
+
+        if (!userProfile) {
+            return res.status(404).json({ error: 'Usuário não encontrado.'});
+        }
+
+        res.json(userProfile)
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar o perfil.' });
+    }
+});
+
+const createUserSchema = z.object({
+    username: z.string().min(4).regex(/^[a-z0-9_]+$/, "Username inválido."),
+    nickname: z.string().min(1, "O nome é obrigatório."),
+    email: z.string().email("Email inválido."),
+    password: z.string().min(8, "A senha precisa ter no mínimo 8 caracteres.")
+});
+
+app.post('/api/users/signup', async (req: Request, res: Response) => {
+    try {
+        const { username, nickname, email, password } = createUserSchema.parse(req.body);
+
+        const existingUser = await prisma.user.findFirst({
+            where: { 
+                OR: [
+                    { email: email },
+                    { username: username }
+                ]
+            }
+        });
+
+        if (existingUser) {
+            if (existingUser.email === email) {
+                return res.status(409).json({ error: "Este email já está em uso." });
+            }
+            if (existingUser.username === username) {
+                return res.status(409).json({ error: "Este username já está em uso." });
+            }
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
         const newUser = await prisma.user.create({
             data: {
-                name,
+                username,
+                nickname,
                 email,
-                password,
+                password: hash,
             }
         });
 
@@ -88,13 +148,52 @@ app.post('/api/users', async (req: Request, res: Response) => {
             message: 'Usuário criado com sucesso',
             user: {
                 id: newUser.id,
-                name: newUser.name,
+                username: newUser.username,
+                nickname: newUser.nickname,
                 email: newUser.email
             }
         });
-    } catch (error) {
+    } catch (error: any) {
+
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({ errors: error.flatten().fieldErrors })
+        }
         console.error(error);
         res.status(500).json({error: 'Erro ao criar usuário.'})
+    }
+});
+
+app.post('/api/login', async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "Formulário não preenchido." });
+    }
+
+    try {
+        const user = await prisma.user.findUnique({ where: { email } });
+        
+        if (!user) {
+            return res.status(401).json({ error: 'Credenciais inválidas.' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Credenciais inválidas.' });
+        }
+
+        const token = jwt.sign(
+            {id : user.id, name: user.name },
+            process.env['JWT_SECRET'] as string,
+            { expiresIn: '7d' }
+        );
+
+        res.json({
+            message: "Login bem-sucedido!",
+            token: token
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao fazer login.' });
     }
 });
 
@@ -128,7 +227,7 @@ app.post('/api/records', async (req: Request, res: Response) => {
 
             include: {
                 song: true,
-                user: {select: {name: true} }
+                user: {select: {username: true} }
             }
         });
 
