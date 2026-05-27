7299dab4eb0c5a8e096a3888f5e3d7188a8cc625
Felipe  Vieira Gualda Pereira <felipegualda@id.uff.br>
2026-05-27 17:53:03 -0300

    feat: Exibir records do usuário em seu perfil

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -53,31 +53,70 @@ export class UserController {
     async getUserRecords(req: Request, res: Response) {
         const { username } = req.params;
 
-        try {
-            const userProfile = await prisma.user.findUnique({
-                where: { username },
-                select: {
-                    id: true,
-                    nickname: true,
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = 10;
+        const skip = (page - 1) * limit
 
-                    records: {
-                        include: {
-                            song: true
-                        },
-                        orderBy: {
-                            registeredAt: 'desc'
+        try {
+            const records = await prisma.record.findMany({
+                where: {
+                    user: {
+                        username: username
+                    }
+                },
+                take: limit,
+                skip: skip,
+                orderBy: {
+                    registeredAt: 'desc'
+                },
+                include: {
+                    song: true,
+                    user: {
+                        select: {
+                            username: true,
+                            nickname: true
                         }
                     }
                 }
             });
 
-            if (!userProfile) {
-                return res.status(404).json({ error: 'Usuário não encontrado.'});
-            }
+            const formattedRecords = records.map((record) => {
+                return {
+                    id: record.id, 
+                    comment: record.comment,
+                    
+                    user: {
+                        username: record.user.username,
+                        nickname: record.user.nickname,
+                    },
 
-            res.json(userProfile)
+                    song: {
+                        spotifyId: record.song.spotifyId,
+                        title: record.song.title,
+                        artist: record.song.artist,
+                        albumImage: record.song.albumImage,
+                        previewUrl: record.song.previewUrl
+                    }
+                }
+            });
+
+            const totalRecords = await prisma.record.count({
+                where: {
+                    user: {
+                        username: username
+                    }
+                }
+            });
+
+            const hasMore = skip + records.length < totalRecords;
+
+            return res.status(200).json({
+                records: formattedRecords,
+                hasMore: hasMore
+            });
         } catch (error) {
-            res.status(500).json({ error: 'Erro ao buscar o perfil.' });
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao buscar os records do usuário.' });
         }
     }
 
